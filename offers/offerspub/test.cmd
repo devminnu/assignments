883b875d721d10abb2119cde4d771018c48afa21
Quazi Minhajuddin <minhajqazi@live.com>
2021-01-18 04:11:47 +0530

    removed unused files

--- offers/offerspub/test.cmd
+++
@@ -1,7 +0,0 @@
-docker image build -t devminnu/offerspub .
-
-docker container run -d -p 30003:30003 --name offerspub devminnu/offerspub
-
-docker container rm -f offerspub
-
-docker push devminnu/offerspub
